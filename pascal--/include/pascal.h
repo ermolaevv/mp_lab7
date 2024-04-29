0d5f2345e6541d62cb832239541b2d373711559e
Cufeed <gusman3000@mail.ru>
2024-04-29 21:13:30 +0300

    Pascal v1.2

--- pascal--/include/pascal.h
+++ pascal--/include/pascal.h
@@ -23,79 +23,45 @@ namespace Pascal {
         std::regex blockRegex("(program|begin|var|const|end|end.)");
         std::regex commentRegex("(//|\\(\\*)");
         std::smatch match;
+        bool tb = false;
+        bool db = false;
         auto it = list.begin();
         while (std::getline(iss, line)) {
             if (std::regex_search(line, match, commentRegex)) {
-                std::cout << "!Comment | " << line << std::endl;
+                /*std::cout << "!Comment | " << line << std::endl;*/
                 continue;
             }
-            if (std::regex_search(line, match, blockRegex)) {
-                it++;
-                std::cout << "Horizont | " << line << std::endl;
-                list.insertAtHorizon(line);
+            if (std::regex_search(line, match, blockRegex) && tb == false) {
+                /*std::cout << "Horizont | " << line << std::endl;*/
+                    list.insertAtHorizon(line);
+                    tb = true;
+                    it = list.begin();
+                    db = false;
+            }
+            else if (std::regex_search(line, match, blockRegex) && tb == true) {
+                /*std::cout << "Horizont | " << line << std::endl;*/
+                    list.insertAtHorizon(line, it.getNode());
+                    db = false;
+                    it++;
             }
             else {
-                /*HierarchicalList<std::string>::spNode firstNode = it.getNode();*/
-                /*auto tit = firstNode;*/
-                std::cout << "Depth | " << line << std::endl;
-                list.insertAtDepth(line, it.getNode()->Down);
+                if (db == false) {
+                    /*HierarchicalList<std::string>::spNode firstNode = it.getNode();*/
+                    /*auto tit = firstNode;*/
+                    /*std::cout << "Depth | " << line << std::endl;*/
+                    list.insertAtDepth(line, it.getNode());
+                    it.Up();
+                }
+                if (db == true) {
+                    /*std::cout << "Depth | " << line << std::endl;*/
+                    list.insertAtHorizon(line, it.getNode()->Down);
+                    /*it.Up();*/
+                }
+                db = true;
             }
         }
     }
 
-//    HierarchicalList<int> myList;
-//
-//    int value1 = 1;
-//    int value2 = 2;
-//    int value3 = 3;
-//
-//    myList.insertAtHorizon(value1);
-//    myList.insertAtHorizon(value2);
-//    myList.insertAtHorizon(value3);
-//
-//    auto it = myList.begin();
-//    HierarchicalList<int>::spNode firstNode = it.getNode();
-//    int value10 = 10;
-//    myList.insertAtDepth(value10, firstNode);
-//    int value20 = 20;
-//    myList.insertAtDepth(value20, firstNode);
-//    int value30 = 30;
-//    myList.insertAtDepth(value30, firstNode);
-//
-//    ASSERT_EQ(myList.size(), 6);
-//
-//    int expectedValues[] = { 30, 20, 10 };
-//    size_t index = 0;
-//    auto depthIt = firstNode->Down;
-//    while (depthIt) {
-//        EXPECT_EQ(depthIt->Value, expectedValues[index++]);
-//        depthIt = depthIt->Next;
-//    }
-//}
-
-    //void parseToHierarchicalList(const std::string& text, HierarchicalList<std::string>& list) {
-    //    std::istringstream iss(text);
-    //    std::string line;
-    //    std::regex blockRegex("(program|begin|var|const|end|end.)");
-    //    std::regex commentRegex("(//|\\(\\*)"); // Регулярное выражение для определения комментариев
-    //    std::smatch match;
-    //    while (std::getline(iss, line)) {
-    //        if (std::regex_search(line, match, commentRegex)) {
-    //            std::cout << "!Comment | " << line << std::endl;
-    //            // Пропускаем комментарии
-    //            continue;
-    //        }
-    //        if (std::regex_search(line, match, blockRegex)) {
-    //            std::cout << "Horizont | " << line << std::endl;
-    //            list.insertAtHorizon(line);
-    //        }
-    //        else {
-    //            // Добавляем целую строку кода в иерархический список
-    //            std::cout << "Depth | " << line << std::endl;
-    //            list.insertAtDepth(line);
-    //        }
-    //    }
-    //}
     /// <summary>
     /// Парсит блок констант (const) из иерархического списка в таблицу констант
     /// Если блок отсутствует, ничего не делать
@@ -103,27 +69,115 @@ namespace Pascal {
     /// <param name="list">Список, содержащий код</param>
     /// <param name="constants">Таблица констант</param>
     void parseVar(HierarchicalList<std::string>& list, OrderedTable<std::string, std::any>& variables) {
-        bool inVarBlock = false;
-        for (auto it = list.begin(); it != list.end(); ++it) {
+        auto it = list.begin();
+        while (it != list.end()) {
             if (it.getNode()->Value == "var") {
-                inVarBlock = true;
+                it.Down();
+                while (it != list.end() && it.getNode()->Value != "begin") {
+                    std::string tmp = it.getNode()->Value;
+                    // Очистка от пробелов, кроме тех, которые в кавычках
+                    bool delete_spaces = true, start = false;
+                    std::string new_str = "";
+                    for (unsigned int i = 0; i < tmp.size(); ++i) {
+                        if (tmp[i] == '\"') {
+                            start ? start = false : start = true;
+                            if (start) {
+                                delete_spaces = false;
+                            }
+                        }
+                        if (!start) {
+                            delete_spaces = true;
+                        }
+                        if (delete_spaces) {
+                            if (tmp[i] != ' ') {
+                                new_str += tmp[i];
+                            }
+                        }
+                        else {
+                            new_str += tmp[i];
+                        }
+                    }
+                    //-------------------------------------------------------
+                    std::istringstream iss(new_str);
+                    // Извлечение типа данных и проверка на =
+                    char tch = ':';
+                    std::size_t pos = new_str.find(tch);
+                    std::string subString = new_str.substr(pos + 1);
+                    subString.pop_back(); // Удаление ;
+                    pos = new_str.find('=');
+                    bool result = false;
+                    std::string tresult;
+                    if (pos != std::string::npos) {
+                        result = true;
+                        tresult = new_str.substr(pos+1);
+                        tresult.pop_back();
+                        pos = subString.find('=');
+                        subString = subString.substr(0, pos);
+                    }
+                    // -----------------------------------------------------
+                    std::string var;
+                    std::string tmpstr = iss.str();
+                    std::size_t tpos = tmpstr.find(':');
+                    tmpstr = tmpstr.substr(0,tpos);
+                    /*while (std::getline(iss, var, ':')) {*/
+                        std::istringstream iss_var(tmpstr);
+                        std::string var_name;
+                        std::string type = subString;
+                        while (std::getline(iss_var, var_name, ',')) {
+                            std::any value;
+                            if (result == true) {
+                                if (type == "double") {
+                                    value = std::stod(tresult);
+                                }
+                                else if (type == "integer") {
+                                    value = std::stoi(tresult);
+                                }
+                                else if (type == "boolean") {
+                                    /*if (var_value == "1") // Возможно такого объявления нет в паскале, надо разбираться
+                                        value = true;
+                                    if (var_value == "0")
+                                        value = false;*/
+                                    if (tresult == "True")
+                                        value = true;
+                                    if (tresult == "False")
+                                        value = false;
+                                    else
+                                        value = false;
+                                }
+                                else if (type == "string") {
+                                    value = (tresult);
+                                }
+                                else if (type == "char") {
+                                    value = char(tresult[0]);
+                                }
+                            }
+                            else { // Значение по умолчанию
+                                if (type == "double") {
+                                    value = 0.0;
+                                }
+                                else if (type == "integer") {
+                                    value = 0;
+                                }
+                                else if (type == "boolean") {
+                                    value = false;
+                                }
+                                else if (type == "string") {
+                                    value = "";
+                                }
+                                else if (type == "char") {
+                                    value = "";
+                                }
+                                
+                            }
+                            variables.Insert(var_name, value);
+                        }
+                        //}
+                    it++;
+                }
             }
-            else if (inVarBlock && (it.getNode()->Value == "end" || it.getNode()->Value == "end.")) {
-                inVarBlock = false;
-            }
-            else if (inVarBlock) {
-                std::string name = it.getNode()->Value;
-                variables.Insert(name, std::any());
-            }
+            it++;
         }
     }
-
-
-
-
-
-
-    
 
     /// <summary>
     /// Парсит блок переменных (var) из иерархического списка в таблицу переменных
@@ -132,23 +186,97 @@ namespace Pascal {
     /// <param name="list">Список, содержащий код</param>
     /// <param name="constants">Таблица констант</param>
     void parseConst(HierarchicalList<std::string>& list, OrderedTable<std::string, std::any>& constants) {
-        bool inConstBlock = false;
-        for (auto it = list.begin(); it != list.end(); ++it) {
+        auto it = list.begin();
+        while (it != list.end()) {
             if (it.getNode()->Value == "const") {
-                inConstBlock = true;
+                it.Down();
+                while (it != list.end() && it.getNode()->Value != "begin") {
+                    std::string tmp = it.getNode()->Value;
+                    // Очистка от пробелов, кроме тех, которые в кавычках
+                    bool delete_spaces = true, start = false;
+                    std::string new_str = "";
+                    for (unsigned int i = 0; i < tmp.size(); ++i) {
+                        if (tmp[i] == '\"') {
+                            start ? start = false : start = true;
+                            if (start) {
+                                delete_spaces = false;
+                            }
+                        }
+                        if (!start) {
+                            delete_spaces = true;
+                        }
+                        if (delete_spaces) {
+                            if (tmp[i] != ' ') {
+                                new_str += tmp[i];
+                            }
+                        }
+                        else {
+                            new_str += tmp[i];
+                        }
+                    }
+                    //-------------------------------------------------------
+                    std::istringstream iss(new_str);
+                    // Извлечение типа данных и проверка на =
+                    char tch = ':';
+                    std::size_t pos = new_str.find(tch);
+                    std::string subString = new_str.substr(pos + 1);
+                    subString.pop_back(); // Удаление ;
+                    pos = new_str.find('=');
+                    bool result = false;
+                    std::string tresult;
+                    if (pos != std::string::npos) {
+                        result = true;
+                        tresult = new_str.substr(pos + 1);
+                        tresult.pop_back();
+                        pos = subString.find('=');
+                        subString = subString.substr(0, pos);
+                    }
+                    // -----------------------------------------------------
+                    std::string var;
+                    std::string tmpstr = iss.str();
+                    std::size_t tpos = tmpstr.find(':');
+                    tmpstr = tmpstr.substr(0, tpos);
+                    /*while (std::getline(iss, var, ':')) {*/
+                    std::istringstream iss_var(tmpstr);
+                    std::string var_name;
+                    std::string type = subString;
+                    while (std::getline(iss_var, var_name, ',')) {
+                        std::any value;
+                        if (result == true) { 
+                            if (type == "double") {
+                                value = std::stod(tresult);
+                            }
+                            else if (type == "integer") {
+                                value = std::stoi(tresult);
+                            }
+                            else if (type == "boolean") {
+                                /*if (var_value == "1") // Возможно такого объявления нет в паскале, надо разбираться
+                                    value = true;
+                                if (var_value == "0")
+                                    value = false;*/
+                                if (tresult == "True")
+                                    value = true;
+                                if (tresult == "False")
+                                    value = false;
+                                else
+                                    value = false;
+                            }
+                            else if (type == "string") {
+                                value = (tresult);
+                            }
+                            else if (type == "char") {
+                                value = char(tresult[0]);
+                            }
+                        }
+                        constants.Insert(var_name, value);
+                    }
+                    /* }*/
+                    it++;
+                }
             }
-            else if (inConstBlock && (it.getNode()->Value == "end" || it.getNode()->Value == "end.")) {
-                inConstBlock = false;
-            }
-            else if (inConstBlock) {
-                std::string name = it.getNode()->Value;
-                std::string value = it.getNode()->Next->Value; // Предполагается, что значение константы следует сразу за её объявлением
-                constants.Insert(name, value);
-            }
+            it++;
         }
     }
-
-
 }
 
 #endif
